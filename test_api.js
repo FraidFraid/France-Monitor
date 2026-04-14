fc04e949305c78fe8a92b6a7bea46c5566075b93
FraidFraid <39705554+FraidFraid@users.noreply.github.com>
2026-04-14 18:41:24 +0200

    chore: stop tracking local root files

--- test_api.js
+++
@@ -1,9 +0,0 @@
-import handler from './api/finance/market.js';
-const req = { method: 'GET' };
-const res = {
-  setHeader: () => {},
-  status: (code) => ({
-    json: (data) => console.log(JSON.stringify(data, null, 2))
-  })
-};
-handler(req, res);
